6f2aa6e333e35928165251bb8689f788c4868f42
desmondsol <soltig66@gmail.com>
2022-07-21 01:33:34 +0300

    some gradient added for the side drawer

--- lib/main.dart
+++ lib/main.dart
@@ -69,99 +69,120 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 173, 151, 211),
-        body: Stack(
-          children: <Widget>[
-            SideMenu(
-              onMenuItemSelection: (pageIndex) {
-                swipeAnimationKey.currentState?.hideNavigationDrawer();
-                setState(() {
-                  currentPage = pageIndex;
-                });
-              },
+        //backgroundColor: Color.fromARGB(255, 173, 151, 211),
+        body: Container(
+          // Add box decoration
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.5, 0.7, 0.9],
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+                Colors.deepPurple[700]!,
+                Colors.deepPurple[400]!,
+                Colors.deepPurple[200]!,
+                Colors.deepPurple[100]!,
+              ],
             ),
-            SwipeAnimation(
-                key: swipeAnimationKey,
-                navigationDrawerOpened: (isOpened) {
-                  isNavigationDrawerOpened = isOpened;
-                  if (isNavigationDrawerOpened) {
-                    controller.reverse();
-                  } else {
-                    controller.forward();
-                  }
+          ),
+
+          child: Stack(
+            children: <Widget>[
+              SideMenu(
+                onMenuItemSelection: (pageIndex) {
+                  swipeAnimationKey.currentState?.hideNavigationDrawer();
+                  setState(() {
+                    currentPage = pageIndex;
+                  });
                 },
-                child: Scaffold(
-                    backgroundColor: Colors.deepPurpleAccent[50],
-                    key: _scaffoldKey,
-                    appBar: AppBar(
-                      brightness: Brightness.dark,
-                      leading: IconButton(
-                        icon: AnimatedIcon(
-                          icon: AnimatedIcons.arrow_menu,
-                          progress: animation,
-                        ),
-                        onPressed: () {
-                          if (isNavigationDrawerOpened) {
-                            controller.reverse();
-                            swipeAnimationKey.currentState
-                                ?.hideNavigationDrawer();
-                          } else {
-                            controller.forward();
-                            swipeAnimationKey.currentState
-                                ?.showNavigationDrawer();
-                          }
-                        },
-                      ),
-                      //title: Text("menuItems[currentPage].menuName"),
-                      backgroundColor: Theme.of(context).primaryColor,
-                      actions: <Widget>[
-                        IconButton(
-                          icon: const Icon(
-                            Icons.help_outline_sharp,
+              ),
+              SwipeAnimation(
+                  key: swipeAnimationKey,
+                  navigationDrawerOpened: (isOpened) {
+                    isNavigationDrawerOpened = isOpened;
+                    if (isNavigationDrawerOpened) {
+                      controller.reverse();
+                    } else {
+                      controller.forward();
+                    }
+                  },
+                  child: Scaffold(
+                      backgroundColor: Colors.deepPurpleAccent[50],
+                      key: _scaffoldKey,
+                      appBar: AppBar(
+                        brightness: Brightness.dark,
+                        leading: IconButton(
+                          icon: AnimatedIcon(
+                            icon: AnimatedIcons.arrow_menu,
+                            progress: animation,
                           ),
-                          tooltip: 'Help',
                           onPressed: () {
-                            final snackBar = SnackBar(
-                              content: Text(
-                                'by Solomon T',
-                                textAlign: TextAlign.center,
-                              ),
-                              backgroundColor: Theme.of(context).primaryColor,
-                              //      duration: Duration(milliseconds: 200),
-                            );
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
+                            if (isNavigationDrawerOpened) {
+                              controller.reverse();
+                              swipeAnimationKey.currentState
+                                  ?.hideNavigationDrawer();
+                            } else {
+                              controller.forward();
+                              swipeAnimationKey.currentState
+                                  ?.showNavigationDrawer();
+                            }
                           },
                         ),
-                      ],
-                      // leading: Icon(Icons.access_time_sharp),
-                      // leadingWidth: 70,
-                      title: const Text('UI clone pages'),
-                    ),
-                    body: BottomNavigationWidget()
-                    // SafeArea(
-                    //   child: Center(
-                    //     child: Column(
-                    //       mainAxisAlignment: MainAxisAlignment.center,
-                    //       children: [
-                    //         Text("Welcome to my app"),
-                    //         Text(
-                    //             "swipe right to access the menu to navigate to the other pages"),
-                    //         Container(
-                    //           child: Wrap(
-                    //             children: [
-                    //               Icon(Icons.select_all),
-                    //               Text("Enjoy"),
-                    //               Icon(Icons.select_all),
-                    //             ],
-                    //           ),
-                    //         )
-                    //       ],
-                    //     ),
-                    //   ),
-                    // ),
-                    ))
-          ],
+                        //title: Text("menuItems[currentPage].menuName"),
+                        backgroundColor: Theme.of(context).primaryColor,
+                        actions: <Widget>[
+                          IconButton(
+                            icon: const Icon(
+                              Icons.help_outline_sharp,
+                            ),
+                            tooltip: 'Help',
+                            onPressed: () {
+                              final snackBar = SnackBar(
+                                content: Text(
+                                  'by Solomon T',
+                                  textAlign: TextAlign.center,
+                                ),
+                                backgroundColor: Theme.of(context).primaryColor,
+                                //      duration: Duration(milliseconds: 200),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            },
+                          ),
+                        ],
+                        // leading: Icon(Icons.access_time_sharp),
+                        // leadingWidth: 70,
+                        title: const Text('UI clone pages'),
+                      ),
+                      body: BottomNavigationWidget()
+                      // SafeArea(
+                      //   child: Center(
+                      //     child: Column(
+                      //       mainAxisAlignment: MainAxisAlignment.center,
+                      //       children: [
+                      //         Text("Welcome to my app"),
+                      //         Text(
+                      //             "swipe right to access the menu to navigate to the other pages"),
+                      //         Container(
+                      //           child: Wrap(
+                      //             children: [
+                      //               Icon(Icons.select_all),
+                      //               Text("Enjoy"),
+                      //               Icon(Icons.select_all),
+                      //             ],
+                      //           ),
+                      //         )
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
+                      ))
+            ],
+          ),
         ),
       ),
     );
